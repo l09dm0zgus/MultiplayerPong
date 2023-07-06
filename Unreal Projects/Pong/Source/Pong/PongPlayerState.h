254e26cfe25c7e04706a3de3daeb352c8859f0f2
l09dm0zgus <cx9ps3@gmail.com>
2023-07-06 23:10:39 +0300

    added scores

---
+++ Unreal Projects/Pong/Source/Pong/PongPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "PongPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PONG_API APongPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
